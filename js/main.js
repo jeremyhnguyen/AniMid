a1a2000fb88ddfd05a4292afaded83ba2e57752b
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-04 16:24:52 +0000

    fixed styling on carousel and added scroll function for navbar #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
-// let $navbar = document.querySelector('.navbar');
+const $navbar = document.querySelector('.navbar');
 
-// let startPosition = window.scrollY;
+let previousPosition = window.scrollY;
 
-// $navbar.addEventListener('scroll', function () {
-//   const scrollPosition = window.scrollY;
-//   if (startPosition > scrollPosition) {
-//   } else {
-//     $navbar = '-50px';
-//   }
-//   startPosition = scrollPosition;
-// });
+window.addEventListener('scroll', function () {
+  const currentPosition = window.scrollY;
+  if (previousPosition < currentPosition) {
+    $navbar.classList.add('hidden');
+  } else {
+    $navbar.classList.remove('hidden');
+  }
+  previousPosition = currentPosition;
+});
+
+// console.log(window.scrollY);
+
+// const $track = document.querySelector('.carousel-track');
+// const $leftButton = document.querySelector('.button-left');
+// const $rightButton = document.querySelector('.button-right');
+// const $radioNav = document.querySelector('.carousel-nav');
+
+// const slides = Array.from($track.children);
+// const radios = Array.from($radioNav.children);
+
+// console.log(slides);
+// when click left move slide left
+// when click right move slide right
+// when clicking nav, move to that slide
