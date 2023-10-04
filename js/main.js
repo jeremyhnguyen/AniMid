86c16450b8001bd93199eda6bcd2f7c886327ec1
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-04 23:03:09 +0000

    fixed display of dropdown menu #1

--- js/main.js
+++ js/main.js
@@ -18,25 +18,55 @@ window.addEventListener('scroll', function () {
 // ---function for dropdown---
 
 const $dropdown = document.querySelector('.menu-select');
-const $menuButton = document.querySelector('.navbar');
+const $menuButton = document.querySelector('.menu');
 
-$navbar.addEventListener('click', function () {
-  if (event.target !== $menuButton) {
+$menuButton.addEventListener('click', function () {
+  if (event.target !== $navbar) {
     $dropdown.classList.toggle('hidden');
   }
 });
 
-// console.log(window.scrollY);
+// issue 1
 
-// const $track = document.querySelector('.carousel-track');
-// const $leftButton = document.querySelector('.button-left');
-// const $rightButton = document.querySelector('.button-right');
-// const $radioNav = document.querySelector('.carousel-nav');
+//    create domtree for airing page view
+//    a.append img
+//    p.append title, score, episodes, airing, year
+//    https://api.jikan.moe/v4/seasons/now
+//    append APIData.data[i].images(choose which image)
+//           APIData.data[i].title_english
+//           APIData.data[i].title_japanese
+//           APIData.data[i].score
+//           APIData.data[i].episodes
+//           APIData.data[i].airing
+//           APIData.data[i].year
 
-// const slides = Array.from($track.children);
-// const radios = Array.from($radioNav.children);
+// recreate carousel
+//    give carousel functionality
 
-// console.log(slides);
-// when click left move slide left
-// when click right move slide right
-// when clicking nav, move to that slide
+// issue 2
+// create test page for individual view
+//    copy nav: row: column-halfs of img&text row:container for preview
+
+//    create dom individual page view (using airing+popular+upcoming)
+//    append APIData.data[i].images(choose which image)
+//           APIData.data[i].title/title_english
+//           APIData.data[i].title_japanese
+//           APIData.data[i].synopsis
+//           APIData.data[i].trailer
+
+// issue 3
+//    create domtree for popular/upcoming page views
+//    https://api.jikan.moe/v4/top/anime - popular
+//    https://api.jikan.moe/v4/seasons/upcoming - upcoming
+//        re-use the domtree used for airing page view
+
+// issue 4
+//    function that will select a random individual page view from the available objects in the 3 arrays: airing/popular/upcoming (should be 75 entries)
+//    check all viewswaps:
+//      logo goes to home (on each page)
+//      menu options all go to their respective views
+
+// STRETCH
+//    1. add search icon/bar based on view
+//          search functionality
+//    2. user can favorite shows/create list
