83eb39245acb9c221b8901e7e6cfdd76e01c8a87
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-06 17:21:18 +0000

    organized index.html and main.js for readability; deleted unused image #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-// ---function for navbar scrolling---
-
 const $navbar = document.querySelector('.navbar');
+const $menuButton = document.querySelector('.menu');
+const $dropdown = document.querySelector('.menu-select');
+const $row = document.querySelector('.row');
+
+// ---function for navbar scrolling---
 
 let previousPosition = window.scrollY;
 
@@ -17,9 +20,6 @@ window.addEventListener('scroll', function () {
 
 // ---function for dropdown---
 
-const $dropdown = document.querySelector('.menu-select');
-const $menuButton = document.querySelector('.menu');
-
 $menuButton.addEventListener('click', function () {
   if (event.target !== $navbar) {
     $dropdown.classList.toggle('hidden');
@@ -27,8 +27,6 @@ $menuButton.addEventListener('click', function () {
 });
 
 // ---function for rendering airing page---
-
-const $row = document.querySelector('.row');
 
 function renderAiring(data) {
   const $col5 = document.createElement('div');
@@ -59,7 +57,6 @@ function renderAiring(data) {
   $col5.appendChild($a);
   $a.appendChild($img);
   $col5.appendChild($showInfo);
-
   $showInfo.appendChild($p1);
   $showInfo.appendChild($p2);
   $showInfo.appendChild($p5);
@@ -72,13 +69,13 @@ function renderAiring(data) {
   if (data.score === null) {
     $p5.classList.add('hidden');
   }
-
   if (data.year === null) {
     $p4.classList.add('hidden');
   }
-
   return $col5;
 }
+
+// --- API Request for Airing Season ---
 
 function getAiring() {
   const xhr = new XMLHttpRequest();
@@ -94,46 +91,3 @@ function getAiring() {
 }
 
 getAiring();
-//    create domtree for airing page view
-//    a.append img
-//    p.append title, score, episodes, airing, year
-//    https://api.jikan.moe/v4/seasons/now
-//    append APIData.data[i].images(choose which image)
-//           APIData.data[i].title_english
-//           APIData.data[i].title_japanese
-//           APIData.data[i].score
-//           APIData.data[i].episodes
-//           APIData.data[i].airing
-//           APIData.data[i].year
-
-//    recreate carousel
-//      give carousel functionality
-
-// issue 2
-// create test page for individual view
-//    copy nav: row: column-halfs of img&text row:container for preview
-
-//    create dom individual page view (using airing+popular+upcoming)
-//    append APIData.data[i].images(choose which image)
-//           APIData.data[i].title/title_english
-//           APIData.data[i].title_japanese
-//           APIData.data[i].synopsis
-//           APIData.data[i].trailer
-// ROW FOR THIS VIEW SHOULD HAVE UNIQUE CLASS!!! popular has score p
-
-// issue 3
-//    create domtree for popular/upcoming page views
-//    https://api.jikan.moe/v4/top/anime - popular
-//    https://api.jikan.moe/v4/seasons/upcoming - upcoming
-//        re-use the domtree used for airing page view
-
-// issue 4
-//    function that will select a random individual page view from the available objects in the 3 arrays: airing/popular/upcoming (should be 75 entries)
-//    check all viewswaps:
-//      logo goes to home (on each page)
-//      menu options all go to their respective views
-
-// STRETCH
-//    1. add search icon/bar based on view
-//          search functionality
-//    2. user can favorite shows/create list
