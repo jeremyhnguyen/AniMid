96f271601138b371a66f0a9a5f9667c572c5b6c9
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-06 22:08:23 +0000

    added styling for each category, menu buttons working for each category, rendering and viewswap functions defined for each menu button #2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const $navbar = document.querySelector('.navbar');
 const $menuButton = document.querySelector('.menu');
 const $dropdown = document.querySelector('.menu-select');
-const $row = document.querySelector('.row');
+const $airingrow = document.querySelector('.airing-row');
+const $popularrow = document.querySelector('.popular-row');
+const $upcomingrow = document.querySelector('.upcoming-row');
+
+const $airingview = document.querySelector('.airing-view');
+const $popularview = document.querySelector('.popular-view');
+const $upcomingview = document.querySelector('.upcoming-view');
+const $individualview = document.querySelector('.individual-view');
+
+const $airingA = document.querySelector('#airing-view');
+const $popularA = document.querySelector('#popular-view');
+const $upcomingA = document.querySelector('#upcoming-view');
+const $logoA = document.querySelector('#home-view');
 
 // ---Function for Navbar Scrolling---
 
@@ -56,7 +68,7 @@ function renderAiring(data) {
   $p4.setAttribute('class', 'pYear');
   $p4.textContent = `Year: ${data.year}`;
 
-  $row.appendChild($col5);
+  $airingrow.appendChild($col5);
   $col5.appendChild($a);
   $a.appendChild($img);
   $col5.appendChild($showInfo);
@@ -74,12 +86,12 @@ function renderAiring(data) {
     $p5.classList.add('hidden');
   }
   if (data.year === null) {
-    $p4.textContent = 'Year: 2023';
+    $p4.classList.add('hidden');
   }
   return $col5;
 }
 
-// --- API Request for Airing Season ---
+// ---API Request for Airing Season---
 
 function getAiring() {
   const xhr = new XMLHttpRequest();
@@ -88,7 +100,7 @@ function getAiring() {
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
       const $airing = renderAiring(xhr.response.data[i]);
-      $row.appendChild($airing);
+      $airingrow.appendChild($airing);
     }
   });
   xhr.send();
@@ -96,36 +108,194 @@ function getAiring() {
 
 getAiring();
 
-// View Swap for Airing page
+// ---Function for Rendering Popular Page---
 
-const $airingview = document.querySelector('.airing-view');
-const $popularview = document.querySelector('.popular-view');
-const $upcomingview = document.querySelector('.upcoming-view');
+function renderPopular(data) {
+  const $col5 = document.createElement('div');
+  $col5.setAttribute('class', 'col-5');
+  const $a = document.createElement('a');
+  $a.setAttribute('href', '#');
+  const $img = document.createElement('img');
+  $img.setAttribute('src', data.images.webp.large_image_url);
+  $img.setAttribute('alt', data.title);
+  const $showInfo = document.createElement('div');
+  $showInfo.setAttribute('class', 'show-info');
+  const $p6 = document.createElement('p');
+  $p6.setAttribute('class', 'PTitleENG');
+  $p6.textContent = data.title_english;
+  const $p1 = document.createElement('p');
+  $p1.setAttribute('class', 'pTitle');
+  $p1.textContent = data.title;
+  const $p2 = document.createElement('p');
+  $p2.setAttribute('class', 'pTitleJP');
+  $p2.textContent = data.title_japanese;
+  const $p5 = document.createElement('p');
+  $p5.textContent = `Score: ${data.score}`;
+  const $p3 = document.createElement('p');
+  $p3.setAttribute('class', 'pEpisodes');
+  $p3.textContent = `Episodes: ${data.episodes}`;
+  const $p4 = document.createElement('p');
+  $p4.setAttribute('class', 'pYear');
+  $p4.textContent = `Year: ${data.year}`;
 
-const $airinglink = document.querySelector('#airing-view');
-const $popularlink = document.querySelector('#popular-view');
-const $upcominglink = document.querySelector('#upcoming-view');
+  $popularrow.appendChild($col5);
+  $col5.appendChild($a);
+  $a.appendChild($img);
+  $col5.appendChild($showInfo);
+  $showInfo.appendChild($p6);
+  $showInfo.appendChild($p1);
+  $showInfo.appendChild($p2);
+  $showInfo.appendChild($p5);
+  $showInfo.appendChild($p3);
+  $showInfo.appendChild($p4);
 
-$airinglink.addEventListener('click', viewSwap);
+  if (data.episodes === null) {
+    $p3.textContent = 'Unreleased';
+  }
+  if (data.score === null) {
+    $p5.classList.add('hidden');
+  }
+  if (data.year === null) {
+    $p4.classList.add('hidden');
+  }
+  return $col5;
+}
 
-// function viewSwap(viewName) {
-//   if () {
-//     $popularview.className = 'hidden';
-//     $individualview.className = 'hidden';
-//     $upcomingview.className = 'hidden';
-//     $airingview.classList.remove('class', 'hidden');
-//   } else if (viewNameShown === 'entries') {
-//     $entriesDiv.classList.remove('hidden');
-//     $formDiv.className = 'hidden';
-//   }
-//   data.view = viewNameShown;
-// }
+// ---API Request for Popular Page---
 
-// Event Handler for viewSwap Entries
+function getPopular() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.jikan.moe/v4/top/anime');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (let i = 0; i < xhr.response.data.length; i++) {
+      const $popular = renderPopular(xhr.response.data[i]);
+      $popularrow.appendChild($popular);
+    }
+  });
+  xhr.send();
+}
 
-// $swapAnchor.addEventListener('click', function () {
-//   viewSwap('entries');
-//   $delete.className = 'hidden';
-//   $form.reset();
-//   $photo.src = 'images/placeholder-image-square.jpg';
-// });
+getPopular();
+
+// ---Function for Rendering Upcoming Page---
+
+function renderUpcoming(data) {
+  const $col5 = document.createElement('div');
+  $col5.setAttribute('class', 'col-5');
+  const $a = document.createElement('a');
+  $a.setAttribute('href', '#');
+  const $img = document.createElement('img');
+  $img.setAttribute('src', data.images.webp.large_image_url);
+  $img.setAttribute('alt', data.title);
+  const $showInfo = document.createElement('div');
+  $showInfo.setAttribute('class', 'show-info');
+  const $p6 = document.createElement('p');
+  $p6.setAttribute('class', 'PTitleENG');
+  $p6.textContent = data.title_english;
+  const $p1 = document.createElement('p');
+  $p1.setAttribute('class', 'pTitle');
+  $p1.textContent = data.title;
+  const $p2 = document.createElement('p');
+  $p2.setAttribute('class', 'pTitleJP');
+  $p2.textContent = data.title_japanese;
+  const $p5 = document.createElement('p');
+  $p5.textContent = `Score: ${data.score}`;
+  const $p3 = document.createElement('p');
+  $p3.setAttribute('class', 'pEpisodes');
+  $p3.textContent = `Episodes: ${data.episodes}`;
+  const $p4 = document.createElement('p');
+  $p4.setAttribute('class', 'pYear');
+  $p4.textContent = `Year: ${data.year}`;
+
+  $upcomingrow.appendChild($col5);
+  $col5.appendChild($a);
+  $a.appendChild($img);
+  $col5.appendChild($showInfo);
+  $showInfo.appendChild($p6);
+  $showInfo.appendChild($p1);
+  $showInfo.appendChild($p2);
+  $showInfo.appendChild($p5);
+  $showInfo.appendChild($p3);
+  $showInfo.appendChild($p4);
+
+  if (data.episodes === null) {
+    $p3.textContent = 'Unreleased';
+  }
+  if (data.score === null) {
+    $p5.classList.add('hidden');
+  }
+  if (data.year === null) {
+    $p4.classList.add('hidden');
+  }
+  return $col5;
+}
+
+// ---API Request for Upcoming Page---
+
+function getUpcoming() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.jikan.moe/v4/seasons/upcoming');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (let i = 0; i < xhr.response.data.length; i++) {
+      const $upcoming = renderUpcoming(xhr.response.data[i]);
+      $upcomingrow.appendChild($upcoming);
+    }
+  });
+  xhr.send();
+}
+
+getUpcoming();
+
+// ---View Swap for Airing View---
+
+$airingA.addEventListener('click', viewAiring);
+
+function viewAiring(event) {
+  if (event.target === $airingA) {
+    $popularview.classList.add('hidden');
+    $individualview.classList.add('hidden');
+    $upcomingview.classList.add('hidden');
+    $airingview.classList.remove('hidden');
+  }
+}
+
+// ---View Swap for Popular View---
+
+$popularA.addEventListener('click', viewPopular);
+
+function viewPopular(event) {
+  if (event.target === $popularA) {
+    $popularview.classList.remove('hidden');
+    $individualview.classList.add('hidden');
+    $upcomingview.classList.add('hidden');
+    $airingview.classList.add('hidden');
+  }
+}
+
+// ---View Swap for Upcoming View---
+
+$upcomingA.addEventListener('click', viewUpcoming);
+
+function viewUpcoming(event) {
+  if (event.target === $upcomingA) {
+    $popularview.classList.add('hidden');
+    $individualview.classList.add('hidden');
+    $upcomingview.classList.remove('hidden');
+    $airingview.classList.add('hidden');
+  }
+}
+
+// ---View Swap for Home View---
+
+$logoA.addEventListener('click', viewHome);
+
+function viewHome(event) {
+  if (event.target === $logoA) {
+    $popularview.classList.remove('hidden');
+    $individualview.classList.add('hidden');
+    $upcomingview.classList.remove('hidden');
+    $airingview.classList.remove('hidden');
+  }
+}
