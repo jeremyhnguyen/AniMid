3db6a05e60554597b642064cacf19b3d644b0f39
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-07 01:34:05 +0000

    revised render function for general use, added data-ids to  #2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
+// ---DOM variables---
+
 const $navbar = document.querySelector('.navbar');
 const $menuButton = document.querySelector('.menu');
 const $dropdown = document.querySelector('.menu-select');
 const $airingrow = document.querySelector('.airing-row');
 const $popularrow = document.querySelector('.popular-row');
 const $upcomingrow = document.querySelector('.upcoming-row');
-
 const $airingview = document.querySelector('.airing-view');
 const $popularview = document.querySelector('.popular-view');
 const $upcomingview = document.querySelector('.upcoming-view');
 const $individualview = document.querySelector('.individual-view');
-
 const $airingA = document.querySelector('#airing-view');
 const $popularA = document.querySelector('#popular-view');
 const $upcomingA = document.querySelector('#upcoming-view');
@@ -38,11 +38,12 @@ $menuButton.addEventListener('click', function () {
   }
 });
 
-// ---Function for Rendering Airing Page---
+// ---Function for Rendering Views---
 
-function renderAiring(data) {
+function renderData(data) {
   const $col5 = document.createElement('div');
   $col5.setAttribute('class', 'col-5');
+  $col5.setAttribute('data-id', data.mal_id);
   const $a = document.createElement('a');
   $a.setAttribute('href', '#');
   const $img = document.createElement('img');
@@ -68,7 +69,6 @@ function renderAiring(data) {
   $p4.setAttribute('class', 'pYear');
   $p4.textContent = `Year: ${data.year}`;
 
-  $airingrow.appendChild($col5);
   $col5.appendChild($a);
   $a.appendChild($img);
   $col5.appendChild($showInfo);
@@ -99,67 +99,14 @@ function getAiring() {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
-      const $airing = renderAiring(xhr.response.data[i]);
-      $airingrow.appendChild($airing);
+      const data = renderData(xhr.response.data[i]);
+      $airingrow.append(data);
     }
   });
   xhr.send();
 }
 
 getAiring();
-
-// ---Function for Rendering Popular Page---
-
-function renderPopular(data) {
-  const $col5 = document.createElement('div');
-  $col5.setAttribute('class', 'col-5');
-  const $a = document.createElement('a');
-  $a.setAttribute('href', '#');
-  const $img = document.createElement('img');
-  $img.setAttribute('src', data.images.webp.large_image_url);
-  $img.setAttribute('alt', data.title);
-  const $showInfo = document.createElement('div');
-  $showInfo.setAttribute('class', 'show-info');
-  const $p6 = document.createElement('p');
-  $p6.setAttribute('class', 'PTitleENG');
-  $p6.textContent = data.title_english;
-  const $p1 = document.createElement('p');
-  $p1.setAttribute('class', 'pTitle');
-  $p1.textContent = data.title;
-  const $p2 = document.createElement('p');
-  $p2.setAttribute('class', 'pTitleJP');
-  $p2.textContent = data.title_japanese;
-  const $p5 = document.createElement('p');
-  $p5.textContent = `Score: ${data.score}`;
-  const $p3 = document.createElement('p');
-  $p3.setAttribute('class', 'pEpisodes');
-  $p3.textContent = `Episodes: ${data.episodes}`;
-  const $p4 = document.createElement('p');
-  $p4.setAttribute('class', 'pYear');
-  $p4.textContent = `Year: ${data.year}`;
-
-  $popularrow.appendChild($col5);
-  $col5.appendChild($a);
-  $a.appendChild($img);
-  $col5.appendChild($showInfo);
-  $showInfo.appendChild($p6);
-  $showInfo.appendChild($p1);
-  $showInfo.appendChild($p2);
-  $showInfo.appendChild($p5);
-  $showInfo.appendChild($p3);
-  $showInfo.appendChild($p4);
-
-  if (data.episodes === null) {
-    $p3.textContent = 'Unreleased';
-  }
-  if (data.score === null) {
-    $p5.classList.add('hidden');
-  }
-  if (data.year === null) {
-    $p4.classList.add('hidden');
-  }
-  return $col5;
-}
 
 // ---API Request for Popular Page---
 
@@ -169,67 +116,14 @@ function getPopular() {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
-      const $popular = renderPopular(xhr.response.data[i]);
-      $popularrow.appendChild($popular);
+      const data = renderData(xhr.response.data[i]);
+      $popularrow.appendChild(data);
     }
   });
   xhr.send();
 }
 
 getPopular();
-
-// ---Function for Rendering Upcoming Page---
-
-function renderUpcoming(data) {
-  const $col5 = document.createElement('div');
-  $col5.setAttribute('class', 'col-5');
-  const $a = document.createElement('a');
-  $a.setAttribute('href', '#');
-  const $img = document.createElement('img');
-  $img.setAttribute('src', data.images.webp.large_image_url);
-  $img.setAttribute('alt', data.title);
-  const $showInfo = document.createElement('div');
-  $showInfo.setAttribute('class', 'show-info');
-  const $p6 = document.createElement('p');
-  $p6.setAttribute('class', 'PTitleENG');
-  $p6.textContent = data.title_english;
-  const $p1 = document.createElement('p');
-  $p1.setAttribute('class', 'pTitle');
-  $p1.textContent = data.title;
-  const $p2 = document.createElement('p');
-  $p2.setAttribute('class', 'pTitleJP');
-  $p2.textContent = data.title_japanese;
-  const $p5 = document.createElement('p');
-  $p5.textContent = `Score: ${data.score}`;
-  const $p3 = document.createElement('p');
-  $p3.setAttribute('class', 'pEpisodes');
-  $p3.textContent = `Episodes: ${data.episodes}`;
-  const $p4 = document.createElement('p');
-  $p4.setAttribute('class', 'pYear');
-  $p4.textContent = `Year: ${data.year}`;
-
-  $upcomingrow.appendChild($col5);
-  $col5.appendChild($a);
-  $a.appendChild($img);
-  $col5.appendChild($showInfo);
-  $showInfo.appendChild($p6);
-  $showInfo.appendChild($p1);
-  $showInfo.appendChild($p2);
-  $showInfo.appendChild($p5);
-  $showInfo.appendChild($p3);
-  $showInfo.appendChild($p4);
-
-  if (data.episodes === null) {
-    $p3.textContent = 'Unreleased';
-  }
-  if (data.score === null) {
-    $p5.classList.add('hidden');
-  }
-  if (data.year === null) {
-    $p4.classList.add('hidden');
-  }
-  return $col5;
-}
 
 // ---API Request for Upcoming Page---
 
@@ -239,8 +133,8 @@ function getUpcoming() {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
-      const $upcoming = renderUpcoming(xhr.response.data[i]);
-      $upcomingrow.appendChild($upcoming);
+      const data = renderData(xhr.response.data[i]);
+      $upcomingrow.appendChild(data);
     }
   });
   xhr.send();
