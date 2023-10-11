7a5937ee5e8e0f7f0ad9ea0e5daf7cc67a04d9eb
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-10-11 01:59:25 +0000

    rendering DOM for individual view #3

--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@
 const $navbar = document.querySelector('.navbar');
 const $menuButton = document.querySelector('.menu');
 const $dropdown = document.querySelector('.menu-select');
-const $airingrow = document.querySelector('.airing-row');
-const $popularrow = document.querySelector('.popular-row');
-const $upcomingrow = document.querySelector('.upcoming-row');
-const $airingview = document.querySelector('.airing-view');
-const $popularview = document.querySelector('.popular-view');
-const $upcomingview = document.querySelector('.upcoming-view');
-const $individualview = document.querySelector('.individual-view');
+const $airingRow = document.querySelector('.airing-row');
+const $popularRow = document.querySelector('.popular-row');
+const $upcomingRow = document.querySelector('.upcoming-row');
+const $airingView = document.querySelector('.airing-view');
+const $popularView = document.querySelector('.popular-view');
+const $upcomingView = document.querySelector('.upcoming-view');
+const $individualView = document.querySelector('.individual-view');
 const $airingA = document.querySelector('#airing-view');
 const $popularA = document.querySelector('#popular-view');
 const $upcomingA = document.querySelector('#upcoming-view');
 const $logoA = document.querySelector('#home-view');
+const $individualRow = document.querySelector('individual-row');
 
 // ---Function for Navbar Scrolling---
 
@@ -91,6 +92,50 @@ function renderData(data) {
   return $col5;
 }
 
+// ---Function for Rendering Individual View---
+
+function renderIndividual(data) {
+  const $col2 = document.createElement('div');
+  $col2.setAttribute('class', 'col-half');
+  const $img = document.createElement('img');
+  $img.setAttribute('src', data.images.webp.large_image_url);
+  $img.setAttribute('alt', data.title);
+  const $synopsis = document.createElement('div');
+  $synopsis.setAttribute('class', 'col-half');
+  $synopsis.setAttribute('class', 'synopsis');
+  const $p1 = document.createElement('p');
+  $p1.textContent = data.title_english;
+  const $p2 = document.createElement('p');
+  $p2.textContent = data.title;
+  const $p3 = document.createElement('p');
+  $p3.textContent = data.title_japanese;
+  const $p4 = document.createElement('p');
+  $p4.textContent = `Score: ${data.score}`;
+  const $p5 = document.createElement('p');
+  $p5.textContent = `Episodes: ${data.episodes}`;
+  const $p6 = document.createElement('p');
+  $p6.textContent = `Year: ${data.year}`;
+  const $p7 = document.createElement('p');
+  $p7.textContent = data.synopsis;
+  const $vidRow = document.createElement('div');
+  $vidRow.setAttribute('class', 'video-row');
+
+  $individualRow.appendChild($col2, $synopsis, $vidRow);
+  $col2.appendChild($img);
+  $synopsis.appendChild($p1, $p2, $p3, $p4, $p5, $p6, $p7);
+
+  if (data.episodes === null) {
+    $p3.textContent = 'Unreleased';
+  }
+  if (data.score === null) {
+    $p5.classList.add('hidden');
+  }
+  if (data.year === null) {
+    $p4.classList.add('hidden');
+  }
+  return $individualRow;
+}
+
 // ---API Request for Airing Season---
 
 function getAiring() {
@@ -100,7 +145,7 @@ function getAiring() {
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
       const data = renderData(xhr.response.data[i]);
-      $airingrow.append(data);
+      $airingRow.append(data);
     }
   });
   xhr.send();
@@ -117,7 +162,7 @@ function getPopular() {
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
       const data = renderData(xhr.response.data[i]);
-      $popularrow.appendChild(data);
+      $popularRow.appendChild(data);
     }
   });
   xhr.send();
@@ -134,7 +179,7 @@ function getUpcoming() {
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.data.length; i++) {
       const data = renderData(xhr.response.data[i]);
-      $upcomingrow.appendChild(data);
+      $upcomingRow.appendChild(data);
     }
   });
   xhr.send();
@@ -148,10 +193,10 @@ $airingA.addEventListener('click', viewAiring);
 
 function viewAiring(event) {
   if (event.target === $airingA) {
-    $popularview.classList.add('hidden');
-    $individualview.classList.add('hidden');
-    $upcomingview.classList.add('hidden');
-    $airingview.classList.remove('hidden');
+    $popularView.classList.add('hidden');
+    $individualView.classList.add('hidden');
+    $upcomingView.classList.add('hidden');
+    $airingView.classList.remove('hidden');
   }
 }
 
@@ -161,10 +206,10 @@ $popularA.addEventListener('click', viewPopular);
 
 function viewPopular(event) {
   if (event.target === $popularA) {
-    $popularview.classList.remove('hidden');
-    $individualview.classList.add('hidden');
-    $upcomingview.classList.add('hidden');
-    $airingview.classList.add('hidden');
+    $popularView.classList.remove('hidden');
+    $individualView.classList.add('hidden');
+    $upcomingView.classList.add('hidden');
+    $airingView.classList.add('hidden');
   }
 }
 
@@ -174,10 +219,10 @@ $upcomingA.addEventListener('click', viewUpcoming);
 
 function viewUpcoming(event) {
   if (event.target === $upcomingA) {
-    $popularview.classList.add('hidden');
-    $individualview.classList.add('hidden');
-    $upcomingview.classList.remove('hidden');
-    $airingview.classList.add('hidden');
+    $popularView.classList.add('hidden');
+    $individualView.classList.add('hidden');
+    $upcomingView.classList.remove('hidden');
+    $airingView.classList.add('hidden');
   }
 }
 
@@ -187,17 +232,17 @@ $logoA.addEventListener('click', viewHome);
 
 function viewHome(event) {
   if (event.target === $logoA) {
-    $popularview.classList.remove('hidden');
-    $individualview.classList.add('hidden');
-    $upcomingview.classList.remove('hidden');
-    $airingview.classList.remove('hidden');
+    $popularView.classList.remove('hidden');
+    $individualView.classList.add('hidden');
+    $upcomingView.classList.remove('hidden');
+    $airingView.classList.remove('hidden');
   }
 }
 
 // --- Issue #3 WIP ---
 // HTML/CSS for Individual View
-// Dummy HTML DOM Tree
 // --- Function add event listener to rows (parent) for Rendering Individual View DOM Tree +api request, template literal to add variable to {id}---
 // --- API Request for IDs ---
+// xhr.open('GET', `https://api.jikan.moe/v4/anime/${id}`)
 // Fix tile uniformity and logo horizontal stretch
 // --- View Swap for Individual View --- (update so that idv. view is hidden in other views)
